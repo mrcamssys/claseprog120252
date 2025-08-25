12b074810d1d22faf180312e365044ba814f6a46
mrcamssys <mrcamssys@gmail.com>
2025-08-25 18:53:27 -0500

    actualizacion para diego mendoza

---
+++ Mendoza Diego/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author server
+ */
+//import java.util.*;
+import java.util.Scanner;
+
+public class Mavenproject1 {
+    
+    
+    public static float pendiente(int yf, int yi, int xf, int xi) {
+        float m;
+        m =  (float) (yf - yi) / (xf - xi);
+        return m;
+    }
+    
+    public static float desfaseEcuRec(float m, int xc0, int yc0){
+        float b;
+        b =  (float)yc0 - m* (float)xc0;
+        return b;
+    }
+    
+    public static void main(String[] args) {
+        float valpen, fase;
+        Parabola par = new Parabola();
+        
+        //Scanner scanner = new Scanner(System.in);
+        //int variable = scanner.nextInt();
+        //scanner.close();
+        
+        valpen = pendiente(3,2,4,1);
+        fase=desfaseEcuRec(valpen, 1, 2);
+        
+        String data=par.miprimeraClase();
+        System.out.println(data);
+        
+        par.setParametros(1, 6, 8);
+        
+        float parametro1=par.get_a();
+        
+        System.out.println("x1= "+ par.x1() + "x2= " + par.x2());
+        System.out.println("la pendiente es: "+valpen+" b= " + fase);
+        System.out.println("y = " + valpen + " x + " + fase );
+    }
+}
